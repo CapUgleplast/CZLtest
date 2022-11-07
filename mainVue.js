97356a404c7d02363e380d44e9f8091e2d7f9200
CapUgleplast <44765959+CapUgleplast@users.noreply.github.com>
2022-11-07 19:08:59 +0700

    tablet

--- mainVue.js
+++ mainVue.js
@@ -6,6 +6,7 @@ new Vue({
         transposeVel: 1,
         elvState: true,
         levelEvents: [],
+        tablet: document.getElementsByClassName("levelTablet"),
         lvlDir: 6
     }
     ,
